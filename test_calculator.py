c212443960477f87dd644aff76f063e9ae70ff30
KoreyEnglert <englertk@oregonstate.edu>
2021-06-05 16:15:59 -0700

    added other functions and tests

--- test_calculator.py
+++ test_calculator.py
@@ -6,6 +6,15 @@ class TestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calculator.add(2, 2), 4)
 
+    def test_sub(self):
+        self.assertEqual(calculator.sub(2, 2), 0)
+
+    def test_mul(self):
+        self.assertEqual(calculator.mul(2, 2), 4)
+
+    def test_div(self):
+        self.assertEqual(calculator.div(2, 2), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
